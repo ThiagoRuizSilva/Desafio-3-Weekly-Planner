60c884c5c5468eb6090e4e7544d31aec187060f4
Thiago Ruiz Silva <thiagoruiz397@gmail.com>
2023-06-14 20:49:41 -0300

    Feat: implementando funcionalidade de registrar usuário

--- src/components/Home/Form register/index.jsx
+++ src/components/Home/Form register/index.jsx
@@ -1,7 +1,88 @@
 import { Link } from "react-router-dom";
 import styles from "./index.module.css";
 
+import {auth} from '../../../FirebaseConection'
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 export default function FormRegister() {
+
+const navigate = useNavigate()
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+const [firstName, setFirstName] = useState('');
+const [lastName, setLastName] = useState('');
+const [birthdate, setBirthdate] = useState('');
+const [country, setCountry] = useState('');
+const [city, setCity] = useState('');
+
+
+
+function formatBirthdate(input) {
+  const onlyNumbers = input.replace(/[^\d]/g, ''); // Remove todos os caracteres não numéricos
+  const day = onlyNumbers.slice(0, 2);
+  const month = onlyNumbers.slice(2, 4);
+  const year = onlyNumbers.slice(4, 8);
+
+  let formattedDate = day;
+  if (month.length > 0) {
+    formattedDate += `/${month}`;
+  }
+  if (year.length > 0) {
+    formattedDate += `/${year}`;
+  }
+
+  return formattedDate;
+}
+
+
+
+async function handleRegister(e) {
+  e.preventDefault();
+
+  if (email !== '' && password !== '') {
+    // Verificar a idade mínima
+    const currentDate = new Date();
+    const birthdateInput = new Date(birthdate);
+    const ageDiffMilliseconds = currentDate - birthdateInput;
+    const ageDate = new Date(ageDiffMilliseconds);
+    const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    if (age < 18) {
+      alert('Você deve ter pelo menos 18 anos para se cadastrar.');
+      return;
+    }
+
+    // Resto do código para o registro
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        alert("deu tudo certo")
+        navigate('/register');
+      })
+      .catch(() => {
+        console.log('Erro ao logar');
+      });
+  } else {
+    alert('Preencha todos os campos.');
+  }
+}
+
+function capitalizeFirstLetter(string) {
+  const words = string.split(' ');
+
+  const capitalizedWords = words.map((word) => {
+    if (word === '') {
+      return word;
+    } else {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    }
+  });
+
+  return capitalizedWords.join(' ');
+}
+
+
+
   return (
     <div className={styles.container__form}>
       <div className={styles.container}>
@@ -9,49 +90,50 @@ export default function FormRegister() {
         <p>Please, register to continue</p>
       </div>
 
-      <form className={styles.form_id}>
+      <form className={styles.form_id} onSubmit={handleRegister}>
         <div className="first__name">
           <label htmlFor="meuInput">first name</label>
-          <input type="text" placeholder="Your first name" />
+          <input type="text" placeholder="Your first name" value={firstName} onChange={(e) => setFirstName(capitalizeFirstLetter(e.target.value))} />
         </div>
 
         <div>
           <label htmlFor="meuInput">last name</label>
-          <input type="text" placeholder="Your last name" />
+          <input type="text" placeholder="Your last name" value={lastName} onChange={(e) => setLastName(capitalizeFirstLetter(e.target.value))} />
         </div>
 
         <div>
           <label htmlFor="meuInput">birth date</label>
-          <input type="text" placeholder="MM/DD/YYYY" />
+          <input type="text" placeholder="MM/DD/YYYY"  value={formatBirthdate(birthdate)} onChange={(e) => setBirthdate(e.target.value)}/>
         </div>
 
         <div>
           <label htmlFor="meuInput">Country</label>
-          <input type="text" placeholder="Your Country" />
+          <input type="text" placeholder="Your Country" value={country} onChange={(e) => setCountry(e.target.value)} />
         </div>
 
         <div>
           <label htmlFor="meuInput">City</label>
-          <input type="text" placeholder="Your City" />
+          <input type="text" placeholder="Your City" value={city} onChange={(e) => setCity(e.target.value)} />
         </div>
 
         <div>
           <label htmlFor="meuInput">e-mail</label>
-          <input type="text" placeholder="A valid e-mail here" />
+          <input type="text" placeholder="A valid e-mail here" value={email} onChange={(e) => setEmail(e.target.value)} />
         </div>
 
         <div>
           <label htmlFor="meuInput">password</label>
-          <input type="password" placeholder="Your password" />
+          <input type="password" placeholder="Your password" value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
 
         <div>
           <label htmlFor="meuInput">password</label>
           <input type="password" placeholder="Comfirm your password" />
         </div>
+      <button type="submit" >Register Now</button>
       </form>
 
-      <Link>Register Now</Link>
+      {/* <Link>Register Now</Link> */}
     </div>
   );
 }
--- src/components/Home/Form/index.jsx
+++ src/components/Home/Form/index.jsx
@@ -1,38 +1,66 @@
 import { Link } from "react-router-dom";
 import styles from "./index.module.css";
+
 import { TiLockClosedOutline } from "react-icons/ti";
 import { AiOutlineUser } from "react-icons/ai";
-import { db } from "../../../FirebaseConection";
+import { auth } from "../../../FirebaseConection";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
 
+import { useNavigate } from "react-router-dom";
+
 export default function Form() {
+  const navigate = useNavigate()
  const [email, setEmail] = useState('')
  const [password, setPassword] = useState('')
 
 
+async function handleLogin(e) {
+e.preventDefault()
 
+if(email !== '' && password !== '') {
+  await signInWithEmailAndPassword(auth, email, password)
+  .then(() => {
+    navigate('/register')
+  })
+  .catch(()=> {
+    alert("erro ao logar senha incorretas")
+  })
+
+
+}else {
+  alert("prencha os campo")
+}
+
+}
 
 
   return (
-    <div className={styles.container__form}>
+    <div className={styles.container__form} >
       <div className={styles.container}>
         <h1>Welcome,</h1>
         <p>To continue browsing safely, log in to the network.</p>
-        <form className={styles.container__login}>
+        <form className={styles.container__login} onSubmit={handleLogin}>
           <h2>Login</h2>
-          <div>
-            <input type="text" placeholder="user name" id="user__id" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <AiOutlineUser />
-          </div>
+           <div className={styles.label_float}>
+            <div className={styles.input__container}>
+            <input type="text" placeholder="user name" id="user__id" className={styles.form__input} value={email} onChange={(e) => setEmail(e.target.value)} />
+            <label htmlFor="user__id">
+              <AiOutlineUser />
+            </label>
+            </div>
+          </div> 
 
-          <div className="password__id">
+
+          <div className={styles.password__id}>
             <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             <TiLockClosedOutline size={30} style={{ marginBottom: "-10px" }} />
           </div>
 
-          <Link to="/register">
+          <button type="submit" >Login</button>
+           {/* <Link to="/register">
             Login
-          </Link>
+          </Link> */}
         </form>
       </div>
     </div>
